5a129168af6a63c26044313ae6ded66ae701832f
seanlee0923 <leesinue0923@gmail.com>
2025-05-28 17:00:04 +0900

    클라이언트 등록/삭제 graceful 하게 되는거 확인

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ptest
+
+go 1.24.1
+
+require github.com/seanlee0923/plscli v0.0.0-20250528023732-7c0433e502c6 // indirect
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/seanlee0923/plscli"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+func main() {
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	cfg := plscli.Config("http://172.16.66.128", "1923", "test-deploy")
+	server := plscli.NewClient(cfg)
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := server.RunWithContext(ctx); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	// 종료신호 받기
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	<-sigCh
+	fmt.Println("Shutting down...")
+
+	cancel()
+	wg.Wait()
+}
